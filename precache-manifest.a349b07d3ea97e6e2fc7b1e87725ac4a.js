0110e170c7971f288f7d9fe7400717cccf347cec
PHC <ragnarokcute@gmail.com>
2020-10-10 12:35:02 +0800

    Updates

---
+++ precache-manifest.a349b07d3ea97e6e2fc7b1e87725ac4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a49904fe11f730a55dcf570c410af00",
+    "url": "/AccessFrontendExercise/index.html"
+  },
+  {
+    "revision": "c57c5ab1bda78f357690",
+    "url": "/AccessFrontendExercise/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1425c2eae5862f56ccd",
+    "url": "/AccessFrontendExercise/static/js/2.5cf292b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AccessFrontendExercise/static/js/2.5cf292b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57c5ab1bda78f357690",
+    "url": "/AccessFrontendExercise/static/js/main.b8d4a5d9.chunk.js"
+  },
+  {
+    "revision": "761e830360fc1ebd92f1",
+    "url": "/AccessFrontendExercise/static/js/runtime-main.146ce4a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AccessFrontendExercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
